d6381d84c5597010c9a5b5ad93a5ac79ee497d39
serdargoleli <web.serdargoleli@gmail.com>
2022-08-13 21:54:39 +0300

    proje dosyaları eklendi
    - Nuxt.js 2, axios, tailwind paketleri yüklendi ve dahil edildi
    - Genel config ayarlamaları yapıldı.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
